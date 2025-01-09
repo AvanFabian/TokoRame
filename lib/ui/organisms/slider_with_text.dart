1f2a1a3f622e9ad6dfa99e3039ac3a78b0411c81
Avan Fabian Daniswara <113287159+AvanFabian@users.noreply.github.com>
2025-01-09 19:31:38 +0700

    refactor implement container rounded on slider_with_text

--- lib/ui/organisms/slider_with_text.dart
+++ lib/ui/organisms/slider_with_text.dart
@@ -4,8 +4,8 @@ import '../atoms/button_widget.dart';
 
 class SliderWithText extends StatefulWidget {
   final List<String> images;
-  final List<String> titles; // Tambahkan daftar judul
-  final List<String> descriptions; // Tambahkan daftar deskripsi
+  final List<String> titles;
+  final List<String> descriptions;
   final VoidCallback onButtonPressed;
 
   const SliderWithText({
@@ -21,7 +21,7 @@ class SliderWithText extends StatefulWidget {
 }
 
 class _SliderWithTextState extends State<SliderWithText> {
-  int _currentIndex = 0; // Untuk melacak slider aktif
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -40,25 +40,26 @@ class _SliderWithTextState extends State<SliderWithText> {
                       });
                     },
                     itemBuilder: (context, index) {
-                      return Image.asset(
-                        widget.images[index],
-                        fit: BoxFit.cover,
+                      return Transform.translate(
+                        offset: const Offset(0, -140), // Move the image slightly up (negative Y-axis)
+                        child: Image.asset(
+                          widget.images[index],
+                          fit: BoxFit.fitWidth,
+                        ),
                       );
                     },
                   ),
                   Positioned(
-                    top: 48, // Posisi teks 'Lewati' di kanan atas
+                    top: 48,
                     right: 16,
                     child: GestureDetector(
                       onTap: () {
-                        // Handle aksi 'Lewati'
                         print('Lewati ditekan');
                       },
                       child: const Text(
                         'Lewati',
                         style: TextStyle(
                           fontSize: 16,
-                          // hex
                           color: Color(0xFF434747),
                           fontWeight: FontWeight.bold,
                         ),
@@ -66,7 +67,7 @@ class _SliderWithTextState extends State<SliderWithText> {
                     ),
                   ),
                   Positioned(
-                    bottom: 16, // Posisi indikator di bawah gambar
+                    bottom: 320,
                     left: 0,
                     right: 0,
                     child: Row(
@@ -74,15 +75,13 @@ class _SliderWithTextState extends State<SliderWithText> {
                       children: List.generate(
                         widget.images.length,
                         (index) => Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 4),
-                          width: _currentIndex == index ? 12 : 8,
-                          height: _currentIndex == index ? 12 : 8,
+                          margin: const EdgeInsets.symmetric(horizontal: 2),
+                          width: _currentIndex == index ? 18 : 8,
+                          height: _currentIndex == index ? 4 : 4,
                           decoration: BoxDecoration(
                             shape: BoxShape.rectangle,
                             borderRadius: BorderRadius.circular(2),
-                            color: _currentIndex == index
-                                ? Colors.white
-                                : Colors.grey,
+                            color: _currentIndex == index ? Colors.white : Colors.grey,
                           ),
                         ),
                       ),
@@ -91,49 +90,50 @@ class _SliderWithTextState extends State<SliderWithText> {
                 ],
               ),
             ),
-            Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(32),
-                  topRight: Radius.circular(32),
-                ),
-              ),
-              padding: const EdgeInsets.only(
-                top: 24,
-                left: 24,
-                right: 24,
-                bottom: 72,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextWidget(
-                    text: widget.titles[_currentIndex],
-                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          height: 1.3,
-                        ),
-                  ),
-                  const SizedBox(height: 12),
-                  TextWidget(
-                    text: widget.descriptions[_currentIndex],
-                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          fontSize: 14,
-                          color: Colors.grey[600],
-                          height: 1.5,
-                        ),
-                  ),
-                  const SizedBox(height: 24),
-                  ButtonWidget(
-                    label: 'Selanjutnya',
-                    onPressed: widget.onButtonPressed,
-                  ),
-                ],
+          ],
+        ),
+        Positioned(
+          bottom: 0, // Position the container at the bottom of the Stack
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 300, // Fixed height for the container
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(48),
+                topRight: Radius.circular(48),
               ),
             ),
-          ],
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 36),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextWidget(
+                  text: widget.titles[_currentIndex],
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        height: 1.3,
+                      ),
+                ),
+                const SizedBox(height: 12),
+                TextWidget(
+                  text: widget.descriptions[_currentIndex],
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        fontSize: 14,
+                        color: Colors.grey[600],
+                        height: 1.5,
+                      ),
+                ),
+                const SizedBox(height: 24),
+                ButtonWidget(
+                  label: 'Selanjutnya',
+                  onPressed: widget.onButtonPressed,
+                ),
+              ],
+            ),
+          ),
         ),
       ],
     );
